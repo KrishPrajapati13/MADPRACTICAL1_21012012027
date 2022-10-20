dd98e7c8f5bff97a3482230224dcb51b29bf93bd
UrvaSuthar <urvasuthar2@gmail.com>
2022-10-20 21:40:06 +0530

    Practical 1

---
+++ src/main/kotlin/P1.kt
@@ -0,0 +1,23 @@
+class P1
+fun main()
+{
+    val integer: Int = 8
+    println("Integer : $integer")
+    val float = 1.8F
+    println("Float : $float")
+    val character: Char = 'a'
+    println("Character : $character")
+    val string: String = "KRISH"
+    println("String : $string")
+    val bool: Boolean = true
+    println("Boolean : $bool")
+    val double: Double = 89.3425
+    println("Double : $double")
+    val long: Long = 21012012027
+    println("Long  : $long")
+    val short: Short = -2
+    println("Short : $short")
+    val byte: Byte = 127
+    println("Byte : $byte")
+
+}
